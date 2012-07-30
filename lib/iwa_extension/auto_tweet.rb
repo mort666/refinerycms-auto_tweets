6eac42b9242e1dea1d6c723750535344a0154cc7
Stephen Kapp <mort666@virus.org>
2012-07-30 14:05:25 +0100

    Fixed more v2.0 settings stuff

--- lib/iwa_extension/auto_tweet.rb
+++ lib/iwa_extension/auto_tweet.rb
@@ -4,12 +4,12 @@ module IwaExtension
   module Tweeter
     def self.post_tweet_message(title, url)
       ::Twitter.configure do |config|
-        config.consumer_key = RefinerySetting.get(:twitter_consumer_key)
-        config.consumer_secret = RefinerySetting.get(:twitter_consumer_secret)
-        config.oauth_token = RefinerySetting.get(:twitter_oauth_token)
-        config.oauth_token_secret = RefinerySetting.get(:twitter_oauth_token_secret)
+        config.consumer_key = Refinery::Setting.get(:twitter_consumer_key)
+        config.consumer_secret = Refinery::Setting.get(:twitter_consumer_secret)
+        config.oauth_token = Refinery::Setting.get(:twitter_oauth_token)
+        config.oauth_token_secret = Refinery::Setting.get(:twitter_oauth_token_secret)
       end
-      message = RefinerySetting.get(:twitter_message)
+      message = Refinery::Setting.get(:twitter_message)
       message = message.gsub('{title}', title).gsub('{url}', url)
       ::Twitter.update(message)
     end
